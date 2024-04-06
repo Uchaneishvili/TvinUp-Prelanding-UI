504b479b5655a00d8b8966774e42f77897d944eb
Giga <g.uchaneishvili>
2024-04-06 16:16:00 +0400

    sending email function is  added

--- src/services/SubscribeService.js
+++ src/services/SubscribeService.js
@@ -3,3 +3,7 @@ import RequestHelper from '../util/RequestHelper'
 export const subscribe = async (body) => {
   return await RequestHelper.beesbid.post(`/subscribe`, body)
 }
+
+export const sendEmailViaMailchimp = async (body) => {
+  return await RequestHelper.beesbid.post(`/sendEmail`, body)
+}
